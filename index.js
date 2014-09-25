1e8814dc408f6a5687ffe13e4017e54f1f6b6f75
Michael Bleigh <mbleigh@mbleigh.com>
2014-09-25 15:53:06 -0400

    Abstracts Client#connect to make it intentional.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var io = require('socket.io-client');
 var Promise = require('promise');
 
-function WDWSClient(url) {
-  this.io = io(url);
+function WDWSClient(url, options) {
+  this.url = url;
+  this.options = options || {};
+}
+
+WDWSClient.prototype.connect = function(callback) {
+  this.socket = io(this.url, this.options.socket || {});
+  if (callback) { this.socket.on('connect', callback); };
 }
 
 WDWSClient.prototype.run = function(cmd, params, callback) {
+  if (!this.socket) { throw "ERROR: Must run connect() before executing commands." };
   var that = this;
   return new Promise(function(resolve, reject) {
-    that.io.emit(cmd, params, function(err) {
+    that.socket.emit(cmd, params, function(err) {
       var rest = Array.prototype.slice.call(arguments, 1);
       err ? reject(err) : resolve.apply(null, rest);
     });
